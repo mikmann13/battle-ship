4a790a122e28d7052469fc079786c86a73f3934d
numList <35351270+numList@users.noreply.github.com>
2021-11-30 22:25:16 -0800

    Update checkers_starter.py

--- checkers_starter.py
+++ checkers_starter.py
@@ -2,14 +2,8 @@ class Board:
     def __init__(self):
         self.width = 8
         self.height = 8
-        self.data = [[' ' for i in range(self.width)] for j in range(self.height)]
-    def can_make_move(self,r,c):
-        if self.data[r][c] == ' ':
-            return True 
-        return False
-    def make_move(self, r, c):
-        if self.can_make_move(r,c):
-            self.data[r][c] = 'S'
+        self.data = [['O' for i in range(self.width)] for j in range(self.height)]
+    
     def __repr__(self):
         s = ''                          
         for row in range(0, self.height):
@@ -18,3 +12,87 @@ class Board:
                 s += self.data[row][col] + 'O'
             s += '\n'
         return s
+
+    def can_place(self,r,c):
+        if self.data[r][c] == 'O':
+            return True 
+        return False
+
+    def place_ship(self, r_0,c_0,r_1,c_1):
+        if r_0 == r_1:
+            for i in range(c_0,c_1):
+                if self.can_place(r_0,i):
+                    self.data[r_0][i] = 'S'
+        if c_0 == c_1:
+            for i in range(r_0,r_1):
+                if self.can_place(i,c_0):
+                    self.data[i][c_0] = 'S'
+
+
+    def init_game(self):
+        while True:
+            r_0 = int(input('start row for 5: '))
+            c_0 = int(input('start col: '))
+            r_1 = int(input('end row: '))
+            c_1 = int(input('end col: '))
+            if r_0 == r_1:
+                if abs(c_0-c_1) == 5:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            if c_0 == c_1:
+                if abs(r_0-r_1) == 5:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            print(self)
+
+            r_0 = int(input('start row for 4: '))
+            c_0 = int(input('start col: '))
+            r_1 = int(input('end row: '))
+            c_1 = int(input('end col: '))
+            if r_0 == r_1:
+                if abs(c_0-c_1) == 4:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            if c_0 == c_1:
+                if abs(r_0-r_1) == 4:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            print(self)
+
+            r_0 = int(input('start row for 3: '))
+            c_0 = int(input('start col: '))
+            r_1 = int(input('end row: '))
+            c_1 = int(input('end col: '))
+            if r_0 == r_1:
+                if abs(c_0-c_1) == 3:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            if c_0 == c_1:
+                if abs(r_0-r_1) == 3:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            print(self)
+            
+            r_0 = int(input('start row for 3: '))
+            c_0 = int(input('start col: '))
+            r_1 = int(input('end row: '))
+            c_1 = int(input('end col: '))
+            if r_0 == r_1:
+                if abs(c_0-c_1) == 3:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            if c_0 == c_1:
+                if abs(r_0-r_1) == 3:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            print(self)
+            
+            r_0 = int(input('start row for 2: '))
+            c_0 = int(input('start col: '))
+            r_1 = int(input('end row: '))
+            c_1 = int(input('end col: '))
+            if r_0 == r_1:
+                if abs(c_0-c_1) == 2:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            if c_0 == c_1:
+                if abs(r_0-r_1) == 2:
+                    self.place_ship(r_0,c_0,r_1,c_1)
+            print(self)
+
+
+
+
+    
+
