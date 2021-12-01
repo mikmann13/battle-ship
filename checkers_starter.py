7638725d7b18025826c946ae928c8b428912622d
numList <35351270+numList@users.noreply.github.com>
2021-11-30 21:13:36 -0800

    Update checkers_starter.py

--- checkers_starter.py
+++ checkers_starter.py
@@ -2,6 +2,11 @@ class Board:
     def __init__(self):
         self.width = 8
         self.height = 8
-        self.data = [[0 for i in range(self.width)] for j in range(self.height)]
-    def is_full(self,r,c):
-        if self.data[r][c]
+        self.data = [[' ' for i in range(self.width)] for j in range(self.height)]
+    def can_make_move(self,r,c):
+        if self.data[r][c] == ' ':
+            return True 
+        return False
+    def make_move(self, r, c):
+        if self.can_make_move(r,c):
+            self.data[r][c] = 'S'
